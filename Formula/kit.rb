130da34332bd0df6823a4b059a03ac9d7dedb4c4
Zander Hill <zander@xargs.io>
2019-08-09 09:34:14 +0100

    Install kit from source

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -3,15 +3,17 @@
 class Kit < Formula
   desc "Binary dependency management - Verify version and SHA256"
   homepage "https://github.com/zph/kit"
-  url "https://github.com/zph/kit/releases/download/v0.9.0/kit-darwin-amd64"
-  sha256 "c19dcdcbd4961a3758443bcacffb6dd5e1c3900ee7084c58ce2a45694890bd2a"
+  url "https://github.com/zph/kit/archive/v0.9.0.tar.gz"
+  sha256 "cad300c54747f6046f4093c1572d026991e8efd466e810b548d30b3effc42c20"
 
-  depends_on "openssl"
+  # depends_on "openssl"
   depends_on "crystal"
 
   def install
-    mv "kit-darwin-amd64", "kit"
-    # At that point there's no need to build releases, just raw compile
+    system "shards install"
+    system "mkdir dist"
+    system "make build"
+    mv "dist/kit-darwin-amd64", "kit"
     bin.install "kit"
   end
 
