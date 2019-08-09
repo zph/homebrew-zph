1c9ba09c35c9a3c327cd7b6492b2bb8cc7223e96
Zander Hill <zander@xargs.io>
2019-08-09 09:25:07 +0100

    Add notes to kit.cr

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -1,3 +1,5 @@
+# Only works from source but requires large download (3GB+ for LLVM)
+# Static linking isn't possible yet with Darwin.
 class Kit < Formula
   desc "Binary dependency management - Verify version and SHA256"
   homepage "https://github.com/zph/kit"
@@ -5,9 +7,11 @@ class Kit < Formula
   sha256 "c19dcdcbd4961a3758443bcacffb6dd5e1c3900ee7084c58ce2a45694890bd2a"
 
   depends_on "openssl"
+  depends_on "crystal"
 
   def install
     mv "kit-darwin-amd64", "kit"
+    # At that point there's no need to build releases, just raw compile
     bin.install "kit"
   end
 
