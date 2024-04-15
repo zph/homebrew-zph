3669ebbd46e5db6b8f9340340a8ca63674ba32e9
Zander Hill <zander@xargs.io>
2024-04-15 10:51:29 -0500

    Switch to pather from deno

--- Formula/pather.rb
+++ Formula/pather.rb
@@ -1,17 +1,18 @@
 class Pather < Formula
   VERSION = "v0.1.0"
-  desc "Toy nim program to dedupe PATH"
+  desc "Toy deno program to dedupe PATH"
   homepage "https://github.com/zph/pather"
   url "https://github.com/zph/pather.git", revision: VERSION
   version VERSION
   sha256 "2682751435097f73a4763a6220d9e5334732795b"
   license "MIT"
 
-  depends_on "nim" => :build
+  depends_on "deno" => :build
 
+  head "https://github.com/zph/pather.git", branch: "main"
   def install
-    system "make", "release-build"
-    bin.install "dist/pather" => "pather"
+    system "deno", "compile", "--allow-env", "--allow-read", "--output", "pather", "main.ts"
+    bin.install "pather" => "pather"
   end
 
   test do
